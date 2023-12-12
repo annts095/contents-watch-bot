645bed15d9fac1aaafd95e0417d89bd4b4e2f873
annts095 <anantasuku@team-lab.com>
2023-12-12 11:04:29 +0900

    feat: implement contests watch

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # contents-watch-bot
 
+## 初期設定
+
+```
+mv env.sample.json env.json
+```
+
+env.json の値を書き換えてください。
+
 ## 実行方法
 
 ```
 sam build
-sam local invoke 
+sam local invoke --env-vars env.json
 ```
--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,20 @@ type Response = {
 
 export const lambdaHandler = async (): Promise<Response> => {
     try {
+        const targetUrl = process.env.TARGET_URL;
+        const targetWord = process.env.TARGET_WORD;
+
+        if (targetUrl === undefined || targetWord === undefined) {
+            throw new Error('TARGET_URL or TARGET_WORD is not defined');
+        }
+
+        const response = await fetch(targetUrl);
+
+        const responseDom = await response.text();
+        if (!responseDom.includes(targetWord)) {
+            console.log('not found target word');
+        }
+
         return {
             statusCode: 200,
             body: JSON.stringify({
@@ -16,7 +30,7 @@ export const lambdaHandler = async (): Promise<Response> => {
         return {
             statusCode: 500,
             body: JSON.stringify({
-                message: 'some error happened',
+                message: err instanceof Error ? err.message : 'unknown error',
             }),
         };
     }
