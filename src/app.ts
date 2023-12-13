aca07b839b6b7c62901b4e0384f0be0e100ed086
annts095 <anantasuku@team-lab.com>
2023-12-13 10:33:05 +0900

    feat: 複数ページの監視に対応

--- src/app.ts
+++ src/app.ts
@@ -5,30 +5,34 @@ type Response = {
 
 export const lambdaHandler = async (): Promise<Response> => {
     try {
-        const targetUrl = process.env.TARGET_URL;
+        const targetUrlArrayJson = process.env.TARGET_URL;
         const targetWord = process.env.TARGET_WORD;
         const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
 
-        if (targetUrl === undefined || targetWord === undefined || slackWebhookUrl === undefined) {
+        if (targetUrlArrayJson === undefined || targetWord === undefined || slackWebhookUrl === undefined) {
             throw new Error('TARGET_URL or TARGET_WORD or slackWebhookUrl is not defined');
         }
 
-        const response = await fetch(targetUrl);
+        const targetUrlArray = JSON.parse(targetUrlArrayJson) as string[];
 
-        const responseDom = await response.text();
-        if (responseDom.includes(targetWord)) {
-            const res = await fetch(slackWebhookUrl, {
-                method: 'POST',
-                body: JSON.stringify({
-                    text: `I found the target!🕵️\n<${targetUrl}|Open in web>`,
-                }),
-            });
-            if (!res.ok) {
-                throw new Error('slack post error');
+        targetUrlArray.forEach(async (targetUrl) => {
+            const response = await fetch(targetUrl);
+
+            const responseDom = await response.text();
+            if (responseDom.includes(targetWord)) {
+                const res = await fetch(slackWebhookUrl, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        text: `I found the target!🕵️\n<${targetUrl}|Open in web>`,
+                    }),
+                });
+                if (!res.ok) {
+                    console.error('slack post error');
+                }
+            } else {
+                console.log('not found target word');
             }
-        } else {
-            console.log('not found target word');
-        }
+        });
 
         return {
             statusCode: 200,
